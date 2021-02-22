d444bd2eb5f86ea9399060486be8b01452ea96ce
VanKhaos <xv50k870!?>
2021-02-22 11:55:57 +0100

    Add Artikel.java

--- src/de/thunderfrog/Artikel.java
+++ src/de/thunderfrog/Artikel.java
@@ -1,4 +1,29 @@
 package de.thunderfrog;
 
 public class Artikel {
+    protected double cost = 0.00;
+    protected final String description;
+
+    public Artikel(String description, double cost){
+        this.cost = cost;
+        this.description = description;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public String toString(){
+        return "Der Artikel " + getDescription() + " hat den Preis: " + getCost();
+    }
+
 }
