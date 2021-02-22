0670bb18f59a1423eeaa485edd89896cd79d828a
VanKhaos <xv50k870!?>
2021-02-22 12:23:02 +0100

    Add Bestellung.java Add Bestelltest.java

--- src/de/thunderfrog/Bestelltest.java
+++ src/de/thunderfrog/Bestelltest.java
@@ -3,5 +3,28 @@ package de.thunderfrog;
 public class Bestelltest {
     public static void main(String[] args) {
 
+        // BESTELLUNG
+        Bestellung bestellung = new Bestellung("Hanuta",0.99,2);
+        System.out.println(bestellung.toString());
+
+        bestellung.setCount(-5);
+        System.out.println(bestellung.toString());
+
+        bestellung.setCost(7.99);
+        System.out.println("Neuer Preis von " + bestellung.getDescription() + " ist " + bestellung.getCost());
+        bestellung.setCount(5);
+        System.out.println(bestellung.toString());
+
+        // ARTIKEL
+        Artikel artikel = new Artikel("Duplo",0.49);
+        System.out.println(artikel.toString());
+        artikel.setCost(0.19);
+        System.out.println("Neuer Preis von " + artikel.getDescription() + " ist " + artikel.getCost());
+        System.out.println(artikel.toString());
+
+        // ARTIKEL BESTELLUNG
+        Bestellung duplo = new Bestellung(artikel.getDescription(), artikel.getCost(),4);
+        System.out.println(duplo.toString());
+
     }
 }
--- src/de/thunderfrog/Bestellung.java
+++ src/de/thunderfrog/Bestellung.java
@@ -1,4 +1,29 @@
 package de.thunderfrog;
 
-public class Bestellung {
+public final class Bestellung extends Artikel{
+
+    protected int count;
+
+    public Bestellung(String description, double cost, int count) {
+        super(description, cost);
+        this.count = count < 0 ? 0 : count;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count < 0 ? 0 : count;
+    }
+
+    public double gesamtpreis(){
+        return cost * count;
+    }
+
+    @Override
+    public String toString(){
+        return count + "x " + description + " kosten: " + gesamtpreis();
+    }
+
 }
